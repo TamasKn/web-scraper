6c6bd3b60690d55b761e4c8208f254d6e4a877d9
TamasKn <tamas.knisz@gmail.com>
2021-03-01 23:05:21 +0100

    Helper updated

--- server/utils/helper.js
+++ server/utils/helper.js
@@ -20,6 +20,31 @@ exports.Helper = {
     },
 
     isValidName(name) {
-        return /^[a-zA-Z]{3,15}$/.test(name)
+        return /^[a-zA-Z]{2,15}$/.test(name)
+    },
+
+    textSanitizer(string) {
+        // Removes any special characters and numbers, new lines
+        // and multiple whitespaces
+        return string
+            .replace(/[^a-zA-Z]/gm, ' ')
+            .replace(/\s\s+/g, ' ')
+    },
+
+    createDictionary(array) {
+        // Creates a hash table with every element of the array
+        // and record their occurrences as lower case to avoid case insensitive duplication
+        // (element must be longer than 2 character)
+        let table = {}
+        for(let el of array) {
+            if(el.length > 2) {
+                if(table[el] !== undefined) {
+                    table[el.toLowerCase()] += 1
+                } else {
+                    table[el.toLowerCase()] = 1
+                }
+            }
+        }
+        return table
     }
 }
